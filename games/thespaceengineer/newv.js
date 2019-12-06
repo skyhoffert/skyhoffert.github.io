c6d32a8908a8fd7e7e29e5e43fbb7231fe6932fa
skyhoffert <skwylarh@yahoo.com>
2019-12-06 12:32:25 -0500

    Added a bolt for testing.

--- games/thespaceengineer/newv.js
+++ games/thespaceengineer/newv.js
@@ -13,14 +13,21 @@ var player = {
     mvR: true,
     size: 64
 };
+var bolty = {
+    x: 720/4,
+    y: 720/4,
+    size: 32,
+    rot: 0.02
+};
 
 // Set scaling to NearestNeighbor to avoid blur.
 PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 // Create the PIXI application.
 const renderer = new PIXI.Renderer({
     view: canvas,
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: 720,
+    height: 720,
+    backgroundColor: 0x666666
 });
 const stage = new PIXI.Container();
 
@@ -38,9 +45,31 @@ guy.anchor.y = 0.5;
 
 stage.addChild(guy);
 
+const boltTex = PIXI.Texture.from("gfx2/bolt.png");
+const bolt = new PIXI.Sprite(boltTex);
+
+bolt.width = bolty.size;
+bolt.height = bolty.size;
+bolt.x = bolty.x;
+bolt.y = bolty.y;
+
+bolt.anchor.x = 0.5;
+bolt.anchor.y = 0.5;
+
+stage.addChild(bolt);
+
 const ticker = new PIXI.Ticker();
 ticker.add(Update);
 ticker.start();
+
+function Collision() {
+    if (guy.x - guy.width/2 < bolty.x &&
+        guy.x + guy.width/2 > bolty.x &&
+        guy.y - guy.height/2 < bolty.y &&
+        guy.y + guy.height/2 > bolty.y) {
+        stage.removeChild(bolt);
+    }
+}
 
 function Update() {
     if (keys["a"]) {
@@ -72,6 +101,10 @@ function Update() {
             player.flipRdy = true;
         }
     }
+
+    bolt.rotation += bolty.rot;
+
+    Collision();
 
     renderer.render(stage);
 }
