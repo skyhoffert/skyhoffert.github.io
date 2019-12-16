117fb7c34c8ea3fdd9bde7b66783831f80a49371
skyhoffert <skwylarh@yahoo.com>
2019-12-15 23:46:09 -0500

    Added basic functionality of tag game!

--- games/tag/tag.js
+++ games/tag/tag.js
@@ -6,9 +6,11 @@
 const WIDTH = 960;
 const HEIGHT = 720;
 const GAME_WIDTH = 720;
-const GAME_BG = "#050505";
-const MENU_BG = "#030303";
-const FPS = 24;
+const GAME_BG = "#222222";
+const MENU_BG = "#181818";
+
+var IP = "100.16.230.232";
+var PORT = "5016";
 
 var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
@@ -18,23 +20,113 @@ canvas.style.width = "133vh";
 canvas.style.height = "100vh";
 
 var players = [];
-
-context.fillStyle = GAME_BG;
-context.fillRect(0, 0, GAME_WIDTH, HEIGHT);
-context.fillStyle = MENU_BG;
-context.fillRect(GAME_WIDTH, 0, WIDTH-GAME_WIDTH, HEIGHT);
-
-context.fillStyle = "red";
-context.fillRect(100, 100, 100, 100);
+var walls = [];
 
 function DrawPlayer(p) {
-    
+    context.fillStyle = p.color;
+    context.beginPath();
+    context.arc(p.x, p.y, p.r, 0, 2*Math.PI);
+    context.closePath();
+    context.fill();
 }
 
 function DrawPlayers() {
     for (let i = 0; i < players.length; i++) {
-        DrawPlayers(players[i]);
+        if (!players[i].active){ continue; }
+        DrawPlayer(players[i]);
     }
 }
 
-DrawPlayers();
+function DrawWalls() {
+    context.fillStyle = "white";
+    for (let i = 0; i < walls.length; i++) {
+        let w = walls[i];
+        context.fillRect(w.x - w.width/2, w.y - w.height/2, w.width, w.height);
+    }
+}
+
+function Draw() {
+    context.fillStyle = GAME_BG;
+    context.fillRect(0, 0, GAME_WIDTH, HEIGHT);
+
+    DrawWalls();
+
+    DrawPlayers();
+    
+    context.fillStyle = MENU_BG;
+    context.fillRect(GAME_WIDTH, 0, WIDTH-GAME_WIDTH, HEIGHT);
+}
+
+Draw();
+
+var wsConnected = false;
+var clientNum = -1;
+var frameCounter = 0;
+var frameRate = 60;
+var ip = IP;
+var keys = {a:false, d:false};
+var ws = new WebSocket("ws://"+ip+":"+PORT);
+
+ws.addEventListener("error", function (err) {
+    if (ip !== IP) {
+        ws = new WebSocket("ws://"+IP+":"+PORT);
+        ip = IP;
+    }
+});
+
+ws.addEventListener("open", function (event) {
+    ws.send(JSON.stringify({"type": "connect"}));
+});
+
+ws.onmessage = function (message) {
+    let obj = JSON.parse(message.data);
+    
+    switch (obj.type) {
+        case "connected":
+            wsConnected = true;
+            clientNum = obj.clientNum;
+            walls = obj.walls;
+            console.log("Successfully connected as client " + clientNum + ".");
+            break;
+        case "tick":
+            players = obj.players;
+            Draw();
+            break;
+        case "tock":
+            if (frameCounter % frameRate == 0) {
+                console.log("Ping: " + (Date.now() - obj.timeSent) + " ms");
+                frameCounter = 0;
+            } else {
+                frameCounter++;
+            }
+            break;
+        default:
+            console.log("Unknown message type.");
+            console.log(obj);
+            break;
+    }
+};
+
+window.onbeforeunload = function () {
+    if (ws) {
+        ws.send(JSON.stringify({"type": "disconnect", "clientNum": clientNum}));
+    }
+}
+
+document.addEventListener("keydown", function (evt) {
+    if (ws.readyState === WebSocket.OPEN &&
+        (evt.key === "a" || evt.key === "d" || evt.key === " " || evt.key === "0")) {
+        keys[evt.key] = true;
+        ws.send(JSON.stringify({type:"tick", timeSent:Date.now(), keys:keys, clientNum:clientNum}));
+    }
+});
+
+document.addEventListener("keyup", function (evt) {
+    if (ws.readyState === WebSocket.OPEN && 
+        (evt.key === "a" || evt.key === "d" || evt.key === " " || evt.key === "0")) {
+        keys[evt.key] = false;
+        ws.send(JSON.stringify({type:"tick", timeSent:Date.now(), keys:keys, clientNum:clientNum}));
+    } else {
+        console.log(evt.key);
+    }
+});
---
+++ games/tag/tag_backend.js
@@ -0,0 +1,153 @@
+/*
+Backend for tag.
+*/
+
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 5016 });
+
+var nClients = 0;
+var players = [];
+var gameWidth = 720;
+var gameHeight = 720;
+var updateRate = 30;
+
+var walls = [];
+walls.push({x:gameWidth/2, y:gameHeight-10, width:gameWidth, height:20});
+walls.push({x:10, y:gameHeight/2, width:20, height:gameHeight});
+walls.push({x:gameWidth-10, y:gameHeight/2, width:20, height:gameHeight});
+walls.push({x:gameWidth/2, y:10, width:gameWidth, height:20});
+
+walls.push({x:gameWidth/2, y:gameHeight-40, width:20, height:40});
+walls.push({x:gameWidth/2, y:gameHeight*3/4, width:gameWidth/3, height:20});
+walls.push({x:gameWidth/2, y:gameHeight/3, width:gameWidth/2, height:20});
+walls.push({x:40, y:gameHeight/2, width:40, height:20});
+walls.push({x:gameWidth-40, y:gameHeight/2, width:40, height:20});
+
+wss.on("connection", function connection(ws) {
+    ws.on("message", function incoming(message) {
+        let obj = JSON.parse(message);
+
+        switch (obj.type) {
+            case "connect":
+                ws.send(JSON.stringify({type:"connected", clientNum:nClients, walls:walls}));
+                players[nClients] = {active:true, x:gameWidth/2, y:gameHeight/2, r:8, color:"red",
+                    ws:ws, keys:{}, speed:0.02, grav:0.01, vx:0, vy:0, maxVx:10, maxVy:10, 
+                    collisions:{}, jumpSpeed:-6, hasJump:false, jFrame:0, jHold:true};
+                nClients++;
+                break;
+            case "disconnect":
+                players[obj.clientNum].active = false;
+                break;
+            case "tick":
+                ws.send(JSON.stringify({"type": "tock", "timeSent": obj.timeSent}));
+                players[obj.clientNum].keys = obj.keys;
+                break;
+            default:
+                console.log("Unknown message type.");
+                console.log(obj);
+                break;
+        }
+    });
+
+    ws.on("close", function close() {
+    })
+});
+
+var prevTime = Date.now();
+
+function Update() {
+    let now = Date.now();
+    let dT = now - prevTime;
+    prevTime = now;
+
+    for (let i=0; i < players.length; i++) {
+        if (!players[i].active){ continue; }
+
+        let p = players[i];
+
+        // Accelerate based on user input.
+        if (p.keys["a"]) {
+            if (!p.collisions["l"]) {
+                p.vx -= p.speed * dT;
+                p.vx = p.vx > p.maxVx ? p.maxVx : p.vx;
+                p.vx = p.vx < -p.maxVx ? -p.maxVx : p.vx;
+                p.collisions["r"] = null;
+            }
+        } else if (p.keys["d"]) {
+            if (!p.collisions["r"]) {
+                p.vx += p.speed * dT;
+                p.vx = p.vx > p.maxVx ? p.maxVx : p.vx;
+                p.vx = p.vx < -p.maxVx ? -p.maxVx : p.vx;
+                p.collisions["l"] = null;
+            }
+        } else {
+            let s = p.speed * dT * Math.sign(p.vx);
+            p.vx = Math.sign(p.vx - s) !== Math.sign(p.vx) ? 0 : p.vx - s;
+        }
+
+        // Gravity.
+        if (p.keys[" "]) {
+            if (p.hasJump) {
+                p.jFrame = 0;
+                p.vy = p.jumpSpeed;
+                p.hasJump = false;
+                p.jHold = true;
+            } else {
+                if (p.jHold && p.jFrame < 2) {
+                    p.jFrame++;
+                    p.vy = p.jumpSpeed * p.jFrame;
+                } else {
+                    p.jHold = false;
+                }
+            }
+        } else {
+            p.jHold = false;
+        }
+        if (!p.jHold) {
+            p.vy += p.grav * dT;
+        }
+        p.vy = p.vy > p.maxVy ? p.maxVy : p.vy;
+
+        // Add velocity to position.
+        p.x += p.vx;
+        if (!p.collisions["c"]) {
+            p.y += p.vy;
+        }
+
+        // Collision.
+        p.collisions["c"] = null;
+        p.collisions["l"] = null;
+        p.collisions["r"] = null;
+        p.collisions["t"] = null;
+        for (let j = 0; j < walls.length; j++) {
+            let w = walls[j];
+            if (p.x > w.x - w.width/2 && p.x < w.x + w.width/2 &&
+                p.y + p.r > w.y - w.height/2 && p.y + p.r < w.y + w.height/2) {
+                p.collisions["c"] = w;
+                p.vy = 0;
+                p.y = w.y - w.height/2 - p.r;
+                p.hasJump = true;
+            } else if (p.x - p.r > w.x - w.width/2 && p.x - p.r < w.x + w.width/2 &&
+                p.y > w.y - w.height/2 && p.y < w.y + w.height/2) {
+                p.collisions["l"] = w;
+                p.vx = 0;
+                p.x = w.x + w.width/2 + p.r;
+            } else if (p.x + p.r > w.x - w.width/2 && p.x + p.r < w.x + w.width/2 &&
+                p.y > w.y - w.height/2 && p.y < w.y + w.height/2) {
+                p.collisions["r"] = w;
+                p.vx = 0;
+                p.x = w.x - w.width/2 - p.r;
+            } else if (p.x > w.x - w.width/2 && p.x < w.x + w.width/2 &&
+                p.y - p.r > w.y - w.height/2 && p.y - p.r < w.y + w.height/2) {
+                p.collisions["t"] = w;
+                p.vy = 0;
+                p.y = w.y + w.height/2 + p.r;
+            }
+        }
+
+        p.ws.send(JSON.stringify({"type": "tick", "players": players}));
+    }
+}
+
+setInterval(Update, 1000/updateRate);
