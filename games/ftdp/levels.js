6854ef1be9869fc926a0b27a0c20550f45d1f1c5
skyhoffert <skwylarh@yahoo.com>
2020-01-13 21:34:26 -0500

    Added more of the level.

--- games/ftdp/levels.js
+++ games/ftdp/levels.js
@@ -7,6 +7,7 @@ const LEVEL_0 = {
     terrain: [
         new RotatedRectangle(-308,-72,40,20,-pi/6, true),
         new RotatedRectangle(330, -68, 185, 80, pi/8, true),
+        new RotatedRectangle(1050, -68, 185, 80, -pi/8, true),
         new Rectangle(0, 30, 800, 200),
         new Rectangle(-600, -330, 400, 800),
         new Rectangle(420, -105, 40, 70),
@@ -14,9 +15,22 @@ const LEVEL_0 = {
         new Rectangle(-360,-80,80,20),
         new Rectangle(690, 30, 580, 200),
         new Rectangle(960,-105,40,70),
+        /*
         new RotatedRectangle(1200, -350, 40, 300, -pi/32),
         new RotatedRectangle(1050, -450, 40, 300, -pi/32),
         new Rectangle(1180, 30, 400, 200),
+        */
+        new RotatedRectangle(1850, -200, 120, 40, pi/10),
+        new RotatedRectangle(1250, -500, 120, 40, -pi/8),
+        new RotatedRectangle(1550, -700, 120, 40, pi/6),
+        new RotatedRectangle(1880, -550, 220, 40, pi/10),
+        
+        new Rectangle(1480, 30, 1000, 200),
+        new Rectangle(2180, -200, 400, 1000),
+        new Rectangle(1550,-310, 200, 40),
+        new Rectangle(1800,-910, 40, 300),
+
+        new Rectangle(2750, -200, 400, 1000),
     ],
     background: [
         new BGRect(2000, -100, 50, 800, 6, "#002200"),
