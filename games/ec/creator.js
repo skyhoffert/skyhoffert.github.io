1d6fe0aae4608fa8976cd29e5214c9edd8ceab6d
skyhoffert <skwylarh@yahoo.com>
2020-08-04 22:38:35 -0400

    ec: Fixed scrolling, clipboard copy works on firefox, added deleting tiles.

--- games/ec/creator.js
+++ games/ec/creator.js
@@ -26,6 +26,7 @@ PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST;
 
 var mouse_selector = {x:0,y:0};
 var mouse_enviro = {x:0,y:0};
+var mouse_in_enviro = false;
 var prev_tile = 0;
 var hover_tile = -1;
 var enviro_tile_idx = {x:0,y:0};
@@ -43,17 +44,25 @@ const selectbox_texture = PIXI.Texture.from("images/selectbox.png");
 
 // Copy exportable dictionary to clipboard.
 function CopyToClipboard() {
-    var data = [new ClipboardItem({ "text/plain": new Blob([JSON.stringify(enviro_tiles_export)], { type: "text/plain" }) })];
-    navigator.clipboard.write(data).then(function() {
-      console.log("Copied to clipboard successfully!");
+    navigator.clipboard.writeText(JSON.stringify(enviro_tiles_export)).then(function() {
+        console.log("Wrote to clipboard!");
     }, function() {
-      console.error("Unable to write to clipboard. :-(");
+        console.error("Could not write to clipboard.")
     });
+
+    return;
 }
 
-// DEBUG: this should not happen on keypress, temporary debugging technique.
 document.addEventListener("keydown", function (evt) {
-    CopyToClipboard();
+    if (evt.key === "s") {
+        CopyToClipboard();
+    }
+    else if (evt.key === "d") {
+        if (enviro_tile_key in enviro_tiles) {
+            viewport.removeChild(enviro_tiles[enviro_tile_key].sprite);
+            delete enviro_tiles[enviro_tile_key];
+        }
+    }
 }, false);
 
 // Global /////////////////////////////////////////////////////////////////////////////////////////
@@ -81,7 +90,6 @@ enviro_app.stage.addChild(viewport);
 viewport
     .drag({mouseButtons: "right"})
     .pinch()
-    .wheel()
     .decelerate();
 
 viewport.moveCenter(0,0);
@@ -91,14 +99,27 @@ viewport.moveCenter(0,0);
 // TODO: bad naming.
 const texture = PIXI.Texture.from("tilesets/terrain_tiles24.png");
 
+// Display which tile position in enviro will be edited.
 const mouse_text = new PIXI.Text("0,0",{ fontFamily: "monospace", fontSize: 24, fill: 0xffffff, align: "center"});
 mouse_text.position.set(10, 10);
 enviro_app.stage.addChild(mouse_text);
 
+// Display which tile in selector is chosen.
 const tile_text = new PIXI.Text("tile 0",{ fontFamily: "monospace", fontSize: 24, fill: 0xffffff, align: "center"});
 tile_text.position.set(10, 44);
 enviro_app.stage.addChild(tile_text);
 
+// Display a little tooltop.
+const tooltip_text = [
+    new PIXI.Text("s: save to clipboard",{ fontFamily: "monospace", fontSize: 12, fill: 0xffffff, align: "center"}),
+    new PIXI.Text("d: delete tile",{ fontFamily: "monospace", fontSize: 12, fill: 0xffffff, align: "center"}),
+];
+for (let i = 0; i < tooltip_text.length; i++) {
+    tooltip_text[i].position.set(10, splitRightElem.offsetHeight - (12*(i+1) + 10));
+    enviro_app.stage.addChild(tooltip_text[i]);
+}
+
+// Hover selectbox for placing tile in enviro.
 const selectbox_hover_env = new PIXI.Sprite(selectbox_texture);
 selectbox_hover_env.anchor.set(0.5);
 selectbox_hover_env.position.set(0, 0);
@@ -124,9 +145,11 @@ const selector_app = new PIXI.Application({
     view: selector_canvas,
 });
 
+// Selectbox outlines which tile will be placed.
 const selectbox = new PIXI.Sprite(selectbox_texture);
 selectbox.position.set(-100, -100);
 
+// Hover indicates which tile the mouse is hovering over currently
 const selectbox_hover = new PIXI.Sprite(selectbox_texture);
 selectbox_hover.position.set(-100, -100);
 selectbox_hover.alpha = 0.5;
@@ -136,12 +159,14 @@ const tiles2 = new PIXI.Sprite(texture);
 tiles2.anchor.set(0.5);
 tiles2.position.set(WIDTH_SELECTOR/2, HEIGHT_SELECTOR/3);
 
+// Will be used later to scale things.
 var scaled_tile_size = 1;
 
 selector_app.stage.addChild(tiles2);
 selector_app.stage.addChild(selectbox_hover);
 selector_app.stage.addChild(selectbox);
 
+// Bounding box for main tiles image.
 var TILES_T = tiles2.y - tiles2.height/2;
 var TILES_L = tiles2.x - tiles2.width/2;
 var TILES_B = tiles2.y + tiles2.height/2;
@@ -149,9 +174,10 @@ var TILES_R = tiles2.x + tiles2.width/2;
 
 var hasDoneLoadProcedure = false;
 
+// LoadProcedure happens later in the code when the texture has been surely loaded.
 function LoadProcedure() {
     // Rescale
-    const newwidth = Math.pow(2, Math.floor(Math.log2(WIDTH_SELECTOR*0.9)));
+    const newwidth = Math.floor(WIDTH_SELECTOR*0.9);
     tmp = tiles2.height / tiles2.width;
     tmp2 = newwidth / tiles2.width;
     tiles2.width = newwidth;
@@ -161,7 +187,7 @@ function LoadProcedure() {
     selectbox_hover.width *= tmp2;
     selectbox_hover.height *= tmp2;
     
-    scaled_tile_size = Math.floor(TILE_SIZE * tmp2);
+    scaled_tile_size = TILE_SIZE * tmp2;
     
     TILES_T = tiles2.y - tiles2.height/2;
     TILES_L = tiles2.x - tiles2.width/2;
@@ -169,6 +195,8 @@ function LoadProcedure() {
     TILES_R = tiles2.x + tiles2.width/2;
 
     selectbox.position.set(TILES_L, TILES_T);
+
+    console.log("Performed load procedure.");
     
     return true;
 }
@@ -187,14 +215,17 @@ selector_app.ticker.add((delta) => {
 // Global /////////////////////////////////////////////////////////////////////////////////////////
 
 document.addEventListener("mousemove", function (evt) {
+    // Right panel actions inside the if.
     if (evt.x > splitLeftElem.offsetWidth) {
         mouse_enviro.x = evt.x;
         mouse_enviro.y = evt.y;
     
         tmp2 = splitLeftElem.offsetWidth;
         tmp = viewport.toWorld(mouse_enviro.x - tmp2, mouse_enviro.y);
-        tmp.x += 16;
-        tmp.y += 16;
+
+        // TODO: this doesn't work correctly. Not sure why.
+        tmp.x += TILE_SIZE;
+        tmp.y += TILE_SIZE/2;
 
         tmp.x = Math.floor(tmp.x / 32);
         tmp.y = Math.floor(tmp.y / 32);
@@ -208,8 +239,14 @@ document.addEventListener("mousemove", function (evt) {
     
         selectbox_hover_env.position.set(enviro_tile_pos.x, enviro_tile_pos.y);
 
+        mouse_in_enviro = true;
+
         return;
     }
+
+    // Left panel actions here.
+
+    mouse_in_enviro = false;
 
     mouse_selector.x = evt.x;
     mouse_selector.y = evt.y;
@@ -221,12 +258,16 @@ document.addEventListener("mousemove", function (evt) {
         hover_tile = Math.floor(tmp / scaled_tile_size) + (Math.floor(tmp2 / scaled_tile_size) * N_TILES_ROW);
         selectbox_hover.x = (hover_tile % 8) * scaled_tile_size + TILES_L;
         selectbox_hover.y = Math.floor(hover_tile / 8) * scaled_tile_size + TILES_T;
+    } else {
+        selectbox_hover.position.set(-100,-100);
     }
 }, false);
 
 document.addEventListener("mousedown", function (evt) {
+    // Only use left mouse button.
     if (evt.button !== 0) { return; }
 
+    // Right panel actions inside if.
     if (evt.x > splitLeftElem.offsetWidth) {
         const x = (prev_tile%8)*32;
         const y = Math.floor(prev_tile/8)*32;
@@ -245,7 +286,7 @@ document.addEventListener("mousedown", function (evt) {
         }
 
         enviro_tiles[enviro_tile_key].sprite.position.set(enviro_tile_pos.x, enviro_tile_pos.y);
-        enviro_tiles[enviro_tile_key].sprite.texture = new PIXI.Texture(texture, new PIXI.Rectangle(x, y, 31, 31));
+        enviro_tiles[enviro_tile_key].sprite.texture = new PIXI.Texture(texture, new PIXI.Rectangle(x, y, 32, 32));
         enviro_tiles[enviro_tile_key].sprite.texture.update();
 
         enviro_tiles_export.tiles[enviro_tile_key] = prev_tile;
@@ -253,10 +294,19 @@ document.addEventListener("mousedown", function (evt) {
         return;
     }
 
+    // Left panel actions here.
+
     if (mouse_selector.x > TILES_L && mouse_selector.x < TILES_R && mouse_selector.y > TILES_T && mouse_selector.y < TILES_B) {
         prev_tile = hover_tile;
         selectbox.x = selectbox_hover.x;
         selectbox.y = selectbox_hover.y;
+    }
+}, false);
+
+// Manually zoom, fixed a buggy viepower.wheel() function.
+document.addEventListener("wheel", function (evt) {
+    if (mouse_in_enviro) {
+        viewport.zoom(evt.deltaY*2, false);
     }
 }, false);
 
