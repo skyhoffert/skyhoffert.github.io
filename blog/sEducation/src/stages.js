d9ea618b0f54084f4b4d5da6227ad6f6749b838a
skyhoffert <skwylarh@yahoo.com>
2022-01-26 16:54:42 -0500

    sEd: Added revision tag to bottom, fixed alignment issues of windows, added map.

--- blog/sEducation/src/stages.js
+++ blog/sEducation/src/stages.js
@@ -14,6 +14,9 @@ class DebugStage extends Stage {
   constructor() {
     super();
 
+    this.AddSprite({id:"map", x: WIDTH*1/3, y: HEIGHT/2, width: 800, 
+      height: 800, filename: "debug_map.png"});
+
     this.AddSprite({id:"player", x: 400, y: 600, width: 100, 
             height: 100, filename: "test_image.png"});
   }
